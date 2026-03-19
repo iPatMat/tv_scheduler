6310a7f3fc9cc4025b4c4c1d25e132b24b264f5f
Patrick Matlick <41339114+iPatMat@users.noreply.github.com>
2026-03-19 00:46:05 -0500

    Update tv_schedule.py

--- tv_schedule.py
+++ tv_schedule.py
@@ -10,7 +10,7 @@ SPORTS = [
     ("basketball",  "nba"),
     ("baseball",    "mlb"),
     ("hockey",      "nhl"),
-    ("basketball",  "mens-college-basketball"),
+    ("basketball",  "mens-college-basketball?groups=50&limit=50"),
     ("football",    "college-football"),
 ]
 
