bcb37965693fa5fc15203c7ead3db5c85cae0506
Patrick Matlick <41339114+iPatMat@users.noreply.github.com>
2026-04-11 19:59:46 -0500

    Update tv_schedule.py

--- tv_schedule.py
+++ tv_schedule.py
@@ -22,7 +22,7 @@ SPORTS = [
     ("basketball",  "mens-college-basketball"),
     ("football",   "college-football"),
     ("soccer", "fifa.world"),
-    ("football", "ufl")
+    ("football", "ufl"),
 ]
 
 def fetch_todays_games():
